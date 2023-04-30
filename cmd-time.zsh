17c482970c878e0e81e46d9e176808448a6319d3
TomfromBerlin <123265893+TomfromBerlin@users.noreply.github.com>
2023-05-01 00:02:34 +0200

    Redraw prompt when terminal size changes

--- cmd-time.zsh
+++ cmd-time.zsh
@@ -18,8 +18,11 @@
 # into your .zshrc, otherwise there will be only zeros as decimal places.      #
 #                                                                              #
 ################################################################################
-# Standardized $0 handling
-# (See https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html)
+
+# Redraw prompt when terminal size changes
+TRAPWINCH() { zle && zle -R }
+
+# Standardized $0 handling (See https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html)
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 typeset -g _CMD_TIME_DIR="${0:A:h}"
