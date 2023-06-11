ef360d80c194ca8b6c3c637897badf44cd101a99
TomfromBerlin <123265893+TomfromBerlin@users.noreply.github.com>
2023-06-11 13:46:41 +0200

    Undo last changes
    ...and what we learned today:
    Don't fix it if it ain't broke.

--- cmd-time.zsh
+++ cmd-time.zsh
@@ -2,7 +2,7 @@
 #                                                                              #
 #        Everything below this line comes with no warranty of any kind.        #
 #                     Use these file at your own risk!                         #
-# last update: 05/08/2023                                                      #
+# last update: 06/2023                                                         #
 ################################################################################
 #                                                                              #
 # This plugin will overwrite your existing RPS1.                               #
@@ -30,26 +30,36 @@ Plugins[cmd-time]="${0:h}"
 TRAPWINCH() {
     zle && zle -R
     }
-_cmd_time_preexec() {
+_command_time_preexec() {
   # check excluded
-    if [[ -n "$ZSH_CMD_TIME_EXCLUDE" ]]; then for exc in $ZSH_CMD_TIME_EXCLUDE; do [ "$(echo "$1" | grep -c "$exc")" -gt 0 ] && RPS1='${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k'
-    return; done
-    timer=${timer:-$SECONDS}
-    timer_show=""
-    export timer_show
-    }
-_cmd_time_precmd() {
-    [[ $timer ]] && timer_show=$(( $SECONDS - $timer ))
-    export timer_show && zsh_cmd_time
+  if [ -n "$ZSH_COMMAND_TIME_EXCLUDE" ]; then
+    cmd="$1"
+    for exc ($ZSH_COMMAND_TIME_EXCLUDE) do;
+      if [ "$(echo $cmd | grep -c "$exc")" -gt 0 ]; then
+        RPS1='${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k'
+        return
+      fi
+    done
+  fi
+  timer=${timer:-$SECONDS}
+  ZSH_COMMAND_TIME_MSG=${ZSH_COMMAND_TIME_MSG-"Time: %s"}
+  ZSH_COMMAND_TIME_COLOR=${ZSH_COMMAND_TIME_COLOR-"white"}
+  export ZSH_COMMAND_TIME=""
+}
+_command_time_precmd() {
+  if [ $timer ]; then timer_show=$(($SECONDS - $timer))
+    if [ -n "$TTY" ] ; then export ZSH_COMMAND_TIME="$timer_show"
+      if [ ! -z ${ZSH_COMMAND_TIME_MSG} ]; then zsh_command_time; fi
+    fi
     unset timer
-    }
-zsh_cmd_time() {
-    if [[ -n "$timer_show" ]]; then
-# we leave the handling of floating point numbers to bc --> https://www.gnu.org/software/bc/manual/html_mono/bc.html
-        h=$(echo "${timer_show}/3600" | bc -l | xargs printf '%f\n')
-        m=$(echo "${timer_show}/3600/60" | bc -l | xargs printf '%f\n')
-        s=$(echo "${timer_show}/60" | bc -l | xargs printf '%f\n')
-        if [[ "$timer_show" -le "1" ]]; then ZSH_CMD_TIME_COLOR="magenta" && timer_show=$(printf '%.6f'" sec" "$timer_show")
+  fi
+}
+zsh_command_time() {
+  if [ -n "$ZSH_COMMAND_TIME" ]; then
+  h=$(bc -l <<< $ZSH_COMMAND_TIME/3600)
+  m=$(bc -l <<< $ZSH_COMMAND_TIME%3600/60)
+  s=$(bc -l <<< $ZSH_COMMAND_TIME%60)
+  if [[ "$timer_show" -le "1" ]]; then ZSH_CMD_TIME_COLOR="magenta" && timer_show=$(printf '%.6f'" sec" "$timer_show")
         elif [[ "$timer_show" -le "60" ]]; then ZSH_CMD_TIME_COLOR="green" && timer_show=$(printf '%.3f'" sec" "$timer_show")
 # '%.nf' defines the number of decimal places, where n is an integer. Values
 # above 14 are possible, but not useful, because the computer's internal
@@ -61,11 +71,11 @@ zsh_cmd_time() {
         elif [[ "$timer_show" -gt "60" ]] && [[ "$timer_show" -le "180" ]]; then ZSH_CMD_TIME_COLOR="cyan" && timer_show=$(printf '%02dm:%02ds' $((m)) $((s)))
         elif [[ "$h" -gt 0 ]]; then m=$((m%60)) && ZSH_CMD_TIME_COLOR="red" && timer_show=$(printf '%02dh:%02dm:%02ds' $((h)) $((m)) $((s))); else ZSH_CMD_TIME_COLOR="yellow" && timer_show=$(printf '%02dm:%02ds' $((m)) $((s)))
         fi
-          elapsed=$(echo -e "%F{$ZSH_CMD_TIME_COLOR}$(printf '%s' "${ZSH_CMD_TIME_MSG}"" $timer_show")%f")
-          export elapsed
-          RPS1='${elapsed} ${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k'
-    fi
+    elapsed=$(echo -e "%F{$ZSH_CMD_TIME_COLOR}$(printf '%s' "${ZSH_CMD_TIME_MSG}"" $timer_show")%f")
+    export elapsed
+    RPS1='${elapsed} ${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k'
+  fi
 }
-precmd_functions+=(_cmd_time_precmd)
-preexec_functions+=(_cmd_time_preexec)
-#
+
+precmd_functions+=(_command_time_precmd)
+preexec_functions+=(_command_time_preexec)
