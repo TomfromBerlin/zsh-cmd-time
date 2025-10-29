d5693c3ebdc97c7f25da4774d98d0f8a57677c8c
TomfromBerlin <123265893+TomfromBerlin@users.noreply.github.com>
2025-10-29 14:33:59 +0100

    Refactor command time tracking variables

--- cmd-time.zsh
+++ cmd-time.zsh
@@ -6,11 +6,10 @@
 ################################################################################
 #                                                                              #
 # This plugin will overwrite your existing RPS1.                               #
-# You can avoid that by replacing the RPS1 definition in line 36 and           #
-# line 60 with your own.                                                       #
-# For the output of the execution time you have to keep ${elapsed} in line 60. #
+# You can avoid that by replacing the RPS1 definition in line 89               #
+# For the output of the execution time you have to keep ${cmd_time_elapsed}   #
 #                                                                              #
-# for the output of decimal places you have to put                             #
+# For the output of decimal places you have to put                             #
 #                                                                              #
 # typeset -F SECONDS                                                           #
 #                                                                              #
@@ -57,22 +56,22 @@ TRAPWINCH() {
 _cmd_time_preexec() {
   # check excluded
     [[ -n "$ZSH_CMD_TIME_EXCLUDE" ]] && for exc in $ZSH_CMD_TIME_EXCLUDE; do [ "$(echo "$1" | grep -c "$exc")" -gt 0 ] && RPS1='${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k' && return; done
-    timer=${timer:-$SECONDS}
-    timer_show=""
-    export timer_show
+    cmd_time_timer=${cmd_time_timer:-$SECONDS}
+    cmd_time_timer_show=""
+    export cmd_time_timer_show
     }
 _cmd_time_precmd() {
-    timer_show=""
-    [[ $timer ]] && timer_show=$(($SECONDS - $timer))
-    export timer_show && zsh_cmd_time
-    unset timer
+    cmd_time_timer_show=""
+    [[ $cmd_time_timer ]] && cmd_time_timer_show=$(($SECONDS - $cmd_time_timer))
+    export cmd_time_timer_show && zsh_cmd_time
+    unset cmd_time_timer
     }
 zsh_cmd_time() {
-    if [[ -n "$timer_show" ]]; then
+    if [[ -n "$cmd_time_timer_show" ]]; then
 # we leave the handling of floating point numbers to bc --> https://www.gnu.org/software/bc/manual/html_mono/bc.html
-        h=$(bc <<< "${timer_show}/3600") && m=$(bc <<< "(${timer_show}%3600)/60") && s=$(bc <<< "${timer_show}%60")
-        if [[ "$timer_show" -le 1 ]]; then ZSH_CMD_TIME_COLOR="magenta" && timer_show=$(printf '%.6f'" sec" "$timer_show")
-        elif [[ "$timer_show" -le 60 ]]; then ZSH_CMD_TIME_COLOR="green" && timer_show=$(printf '%.3f'" sec" "$timer_show")
+        local h=$(bc <<< "${cmd_time_timer_show}/3600") && local m=$(bc <<< "(${cmd_time_timer_show}%3600)/60") && local s=$(bc <<< "${cmd_time_timer_show}%60")
+        if [[ "$cmd_time_timer_show" -le 1 ]]; then ZSH_CMD_TIME_COLOR="magenta" && cmd_time_timer_show=$(printf '%.6f'" sec" "$cmd_time_timer_show")
+        elif [[ "$cmd_time_timer_show" -le 60 ]]; then ZSH_CMD_TIME_COLOR="green" && cmd_time_timer_show=$(printf '%.3f'" sec" "$cmd_time_timer_show")
 # '%.nf' defines the number of decimal places, where n is an integer. Values
 # above 14 are possible, but not useful, because the computer's internal
 # representation of floating point numbers has a limited number of bits and as
@@ -80,12 +79,12 @@ zsh_cmd_time() {
 # stored as e.g. 3.0000000000 in memory, but as 3.0000000002 or 2.9999999998.
 # Rounding errors are therefore unavoidable and you can safely ignore everything
 # after the 14th decimal place in a result.
-        elif [[ "$timer_show" -gt 60 ]] && [[ "$timer_show" -le 180 ]]; then ZSH_CMD_TIME_COLOR="cyan" && timer_show=$(printf '%02dm:%02ds' $((m)) $((s)))
-        elif [[ "$h" -gt 0 ]]; then m=$((m%60)) && ZSH_CMD_TIME_COLOR="red" && timer_show=$(printf '%02dh:%02dm:%02ds' $((h)) $((m)) $((s))); else ZSH_CMD_TIME_COLOR="yellow" && timer_show=$(printf '%02dm:%02ds' $((m)) $((s)))
+        elif [[ "$cmd_time_timer_show" -gt 60 ]] && [[ "$cmd_time_timer_show" -le 180 ]]; then ZSH_CMD_TIME_COLOR="cyan" && cmd_time_timer_show=$(printf '%02dm:%02ds' $((m)) $((s)))
+        elif [[ "$h" -gt 0 ]]; then m=$((m%60)) && ZSH_CMD_TIME_COLOR="red" && cmd_time_timer_show=$(printf '%02dh:%02dm:%02ds' $((h)) $((m)) $((s))); else ZSH_CMD_TIME_COLOR="yellow" && cmd_time_timer_show=$(printf '%02dm:%02ds' $((m)) $((s)))
         fi
-          elapsed=$(echo -e "%F{$ZSH_CMD_TIME_COLOR}$(printf '%s' "${ZSH_CMD_TIME_MSG}"" $timer_show")%f")
-          export elapsed
-          RPS1='${elapsed} ${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k'
+          cmd_time_elapsed=$(echo -e "%F{$ZSH_CMD_TIME_COLOR}$(printf '%s' "${ZSH_CMD_TIME_MSG}"" $cmd_time_timer_show")%f")
+          export cmd_time_elapsed
+          RPS1='${cmd_time_elapsed} ${vcs_info_msg_0_} %(?.%F{green}√.%K{red}%F{black} Nope!)%f%k'
     fi
 }
 precmd_functions+=(_cmd_time_precmd)
@@ -106,4 +105,3 @@ zsh_cmd_time_unload() {
     unset _ZSH_CMD_TIME_LOADED
     unset cmd_start_time cmd_time_display
 }
-
