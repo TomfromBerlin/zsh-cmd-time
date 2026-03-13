f90fd6e969a669640086f702fd86de15301f67d7
TomfromBerlin <123265893+TomfromBerlin@users.noreply.github.com>
2026-03-13 07:54:37 +0100

    remove editor formatting strings

--- cmd-time.plugin.zsh
+++ cmd-time.plugin.zsh
@@ -1,4 +1,3 @@
-# -*- mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4;-*-
 # Standardized $0 handling
 # (See https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html)
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
@@ -10,4 +9,3 @@ if [[ ${zsh_loaded_plugins[-1]} != */zsh-cmd-time && -z ${fpath[(r)${0:h}]} ]] {
     fpath+=( "${0:h}" )
 }
 source ${0:A:h}/cmd-time.zsh
-# vim:ft=zsh:tw=80:sw=4:sts=4:et:foldmarker=[[[,]]]
