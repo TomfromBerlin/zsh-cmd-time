94db41639d4b76909c0e8ee062ced77371cb06c2
TomfromBerlin <tom-at-github@quantentunnel.de>
2026-03-20 22:56:43 +0100

    Cleanup code

--- cmd-time.plugin.zsh
+++ cmd-time.plugin.zsh
@@ -1,11 +1,11 @@
-# Standardized $0 handling
-# (See https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html)
+# Standardized $0 handling (See https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html)
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
-# Set $0 with a new trik - use of %x prompt expansion
+# Set $0 with use of %x prompt expansion
 0="${ZERO:-${${${(M)${0::=${(%):-%x}}:#/*}:-$PWD/$0}:A}}"
 local ZERO="$0"
-# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html#indicator
-if [[ ${zsh_loaded_plugins[-1]} != */zsh-cmd-time && -z ${fpath[(r)${0:h}]} ]] {
-    fpath+=( "${0:h}" )
-}
+typeset -g CMD_TIME_DIR="${0:A:h}"
+# https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
+typeset -gA Plugins
+Plugins[cmd-time]="${0:h}"
+
 source ${0:A:h}/cmd-time.zsh
--- cmd-time.zsh
+++ cmd-time.zsh
@@ -20,15 +20,6 @@
 #             : ${CMD_TIME_EXCLUDE:="^(ls|cd|pwd|clear|exit)$"}                #
 #                                                                              #
 ################################################################################
-# Standardized $0 handling (See https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html)
-0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
-# Set $0 with use of %x prompt expansion
-0="${ZERO:-${${${(M)${0::=${(%):-%x}}:#/*}:-$PWD/$0}:A}}"
-local ZERO="$0"
-typeset -g CMD_TIME_DIR="${0:A:h}"
-# https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
-typeset -gA Plugins
-Plugins[cmd-time]="${0:h}"
 
 autoload -Uz is-at-least
 if ! is-at-least 5.0.5; then
